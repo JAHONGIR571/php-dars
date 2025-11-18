607bcf116912d033491589d94437405d2ad3712f
JAHONGIT571 <jahongirerkinov057@gmail.com>
2025-11-18 15:59:40 +0500

    switch bayonati

--- index.php
+++ index.php
@@ -1,2 +1,35 @@
 <?php
-echo "hello world";
+$a = 7;
+switch ($a){
+case 1;
+echo "bugun dushanba ";
+break;
+
+case 2;
+echo " bugun seshanba ";
+break;
+
+case 3;
+echo " bugun chorshanba ";
+break;
+
+case 4;
+echo "bugun payshanba ";
+break;
+
+case 5;
+echo "bugun juma";
+break;
+
+case 6;
+echo "bugun shanba ";
+break;
+
+case 7;
+echo "bugun yakshanba";
+
+default;
+echo "bu kun mavjud emas";
+
+}
+
